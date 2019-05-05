4b77c35c3fec65022ee73af1c0d41a50544a4f13
Julia Pauletti <julia.pauletti@yahoo.com>
2019-05-04 19:38:03 -0500

    styling changes, icons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
-$(".card").on("hover", function(event) {
-    console.log("hovering");
-    $(".overlay").removeClass("hide");
-})
-
-
-
 $(".card").hover(function (event) {
     console.log("hovering");
     $(this).children(".overlay").toggleClass("hide");
+})
+
+$(".card").on("mouseenter", function (event) {
+    $(this).children("img").css("opacity", "0.7");
+})
+
+$(".card").on("mouseleave", function (event) {
+    $(this).children("img").css("opacity", "1");
 })
